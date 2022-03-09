39f9159b2a5817e26bcaf8a74af5c12c93cca64b
Daniel <danielwebdeveloper220@gmail.com>
2022-03-08 23:21:55 -0500

    goalModel, getGoals, createGoals

--- backend/controllers/goalCtrl.js
+++ backend/controllers/goalCtrl.js
@@ -1,8 +1,12 @@
 const asyncHandler = require("express-async-handler");
 
+const Goal = require("../models/goalModel.js");
+
 // GET
 const getGoals = asyncHandler(async (req, res) => {
-  res.status(200).json({ msg: "get goals" });
+  const goals = await Goal.find();
+
+  res.status(200).json(goals);
 });
 
 // POST
@@ -12,12 +16,27 @@ const createGoal = asyncHandler(async (req, res) => {
     throw new Error("Please add a text field");
   }
 
-  res.status(200).json({ msg: "create goal" });
+  const goal = await Goal.create({
+    text: req.body.text,
+  });
+
+  res.status(200).json(goal);
 });
 
 // UPDATE
 const updateGoal = asyncHandler(async (req, res) => {
-  res.status(200).json({ msg: `update goal ${req.params.id}` });
+  const goal = await Goal.findById(req.params.id);
+
+  if (!goal) {
+    res.status(400);
+    throw new Error("Goal not found");
+  }
+
+  const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+  });
+
+  res.status(200).json(updatedGoal);
 });
 
 // DELETE
