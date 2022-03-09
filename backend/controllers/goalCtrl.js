6fdfbe7558ca74a413728ef86f15939753e200bd
danielWebDev-spec <danielwebdeveloper220@gmail.com>
2022-03-09 00:01:42 -0500

    deleteGoal

--- backend/controllers/goalCtrl.js
+++ backend/controllers/goalCtrl.js
@@ -41,7 +41,18 @@ const updateGoal = asyncHandler(async (req, res) => {
 
 // DELETE
 const deleteGoal = asyncHandler(async (req, res) => {
-  res.status(200).json({ msg: `delete goal ${req.params.id}` });
+  const goal = await Goal.findById(req.params.id);
+
+  if (!goal) {
+    res.status(400);
+    throw new Error("Goal not found");
+  }
+
+  await goal.remove();
+
+  res.status(200).json({
+    id: req.params.id,
+  });
 });
 
 module.exports = { getGoals, createGoal, updateGoal, deleteGoal };
