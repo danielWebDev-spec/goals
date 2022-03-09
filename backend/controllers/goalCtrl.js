878148cb5a6e92876166c447f2ac328bebad2b63
Daniel <danielwebdeveloper220@gmail.com>
2022-03-08 20:41:38 -0500

    Connected to mongodb

--- backend/controllers/goalCtrl.js
+++ backend/controllers/goalCtrl.js
@@ -1,26 +1,28 @@
+const asyncHandler = require("express-async-handler");
+
 // GET
-const getGoals = (req, res) => {
+const getGoals = asyncHandler(async (req, res) => {
   res.status(200).json({ msg: "get goals" });
-};
+});
 
 // POST
-const createGoal = (req, res) => {
+const createGoal = asyncHandler(async (req, res) => {
   if (!req.body.text) {
     res.status(400);
     throw new Error("Please add a text field");
   }
 
   res.status(200).json({ msg: "create goal" });
-};
+});
 
 // UPDATE
-const updateGoal = (req, res) => {
+const updateGoal = asyncHandler(async (req, res) => {
   res.status(200).json({ msg: `update goal ${req.params.id}` });
-};
+});
 
 // DELETE
-const deleteGoal = (req, res) => {
+const deleteGoal = asyncHandler(async (req, res) => {
   res.status(200).json({ msg: `delete goal ${req.params.id}` });
-};
+});
 
 module.exports = { getGoals, createGoal, updateGoal, deleteGoal };
--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const dotenv = require("dotenv").config();
 const { errorHandler } = require("./middleware/errorMiddleware.js");
 
@@ -11,6 +12,20 @@ app.use(express.urlencoded({ extended: false }));
 app.use("/api/goals", require("./routes/goalRoutes.js"));
 
 app.use(errorHandler);
+
+// Connect to mongodb
+const URI = process.env.MONGODB_URL;
+mongoose.connect(URI, (err) => {
+  if (err) throw err;
+  console.log("Connected to MongoDB");
+});
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+  });
+}
 
 // Listener
 const PORT = process.env.PORT || 5000;
