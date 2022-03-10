d38093bdbbd4d3e2564552d76a3857757096212b
danielWebDev-spec <danielwebdeveloper220@gmail.com>
2022-03-10 18:32:22 -0500

    user registration

--- client/src/features/auth/authSlice.js
+++ client/src/features/auth/authSlice.js
@@ -1,4 +1,5 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import authService from "./authService";
 
 // Get user from localStorage
 const user = JSON.parse(localStorage.getItem("user"));
@@ -10,6 +11,22 @@ const initialState = {
   isLoading: false,
   message: "",
 };
+
+// Register user
+export const register = createAsyncThunk(
+  "auth/register",
+  async (user, thunkAPI) => {
+    try {
+      return await authService.register(user);
+    } catch (error) {
+      const message =
+        (error.response && error.response.data && error.response.data.msg) ||
+        error.msg ||
+        error.toString();
+      return thunkAPI.rejectWithValue(message);
+    }
+  }
+);
 
 export const authSlice = createSlice({
   name: "auth",
