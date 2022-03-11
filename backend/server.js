5622622a30d1f31331deb2a457215f3c90e3ea66
danielWebDev-spec <danielwebdeveloper220@gmail.com>
2022-03-11 18:14:15 -0500

    serve frontend client

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+const path = require("path");
 const express = require("express");
 const connectDB = require("./config/db.js");
 const colors = require("colors");
@@ -16,6 +17,19 @@ app.use(express.urlencoded({ extended: false }));
 // routes
 app.use("/api/goals", require("./routes/goalRoutes.js"));
 app.use("/api/users", require("./routes/userRoutes.js"));
+
+// Serve frontend
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(
+      path.resolve(__dirname, "../", "client", "build", "index.html")
+    )
+  );
+} else {
+  app.get("/", (req, res) => res.send("please set to production"));
+}
 
 app.use(errorHandler);
 
