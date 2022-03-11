3d4cf7b757dd6a319d0a4794e09185fae86be860
danielWebDev-spec <danielwebdeveloper220@gmail.com>
2022-03-10 20:28:11 -0500

    backend fixes

--- backend/controllers/goalCtrl.js
+++ backend/controllers/goalCtrl.js
@@ -35,15 +35,14 @@ const updateGoal = asyncHandler(async (req, res) => {
   }
 
   // check for user
-  const user = await User.findById(req.user.id);
 
-  if (!user) {
+  if (!req.user) {
     res.status(401);
     throw new Error("User not found");
   }
 
   // make sure the logged in user matches the goal user
-  if (goal.user.toString() !== user.id) {
+  if (goal.user.toString() !== req.user.id) {
     res.status(401);
     throw new Error("User not authorized");
   }
@@ -65,15 +64,14 @@ const deleteGoal = asyncHandler(async (req, res) => {
   }
 
   // check for user
-  const user = await User.findById(req.user.id);
 
-  if (!user) {
+  if (!req.user) {
     res.status(401);
     throw new Error("User not found");
   }
 
   // make sure the logged in user matches the goal user
-  if (goal.user.toString() !== user.id) {
+  if (goal.user.toString() !== req.user.id) {
     res.status(401);
     throw new Error("User not authorized");
   }
